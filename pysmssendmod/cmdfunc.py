e39acd0e119fe613ccb29c90c2ee8d86701b7a48
Markos Chandras <hwoarang@silverarrow.gr>
2009-02-05 16:03:42 +0200

    Rolling back on web-wise sending messages

--- pysmssendmod/cmdfunc.py
+++ pysmssendmod/cmdfunc.py
@@ -1,176 +1,174 @@
-
-import sys,signal 
-import os
-import getopt
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#***************************************************************************
+#*    Pysmssend  ( Source Code ) Cmdfunc
+#***************************************************************************
+# This file is part of Pysmssend Project
+#
+#    Pysmssend Project is free software; you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation; either version 3 of the License, or
+#     (at your option) any later version.
+#
+#    Pysmssend is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
 from mechanize import Browser 
-
-SHAREDIR="/usr/share/pysmssend/"
-TEMPDIR="/.pysmssend/"
-verbose = False
+from pysmssendmod.usage import *
+from pysmssendmod.creditsleft import *
+import sys
+import urllib2,urllib
 foobar = Browser()
 foobar.set_handle_robots(False)
-
 acc_openlogin = {
-	'otenet':'http://tools.otenet.gr/tools/index.do',
-	'voipbuster':'https://myaccount.voipbuster.com/clx/index.php?part=plogin&username=Znexbf6430&password=',
-	'voipdiscount':'https://myaccount.voipdiscount.com/clx/index.php?part=plogin&username=Znexbf6430&password=',
-	'lowratevoip':'https://myaccount.lowratevoip.com/clx/index.php?part=plogin&username=Znexbf6430&password=',	
-	'voipbusterpro':'https://myaccount.voipbusterpro.com/clx/index.php?part=plogin&username=Znexbf6430&password=',
-	'voipcheap':'https://myaccount.voipcheap.com/clx/index.php?part=plogin&username=Znexbf6430&password=',
-	'freevoip':'https://myaccount.freevoip.com/clx/index.php?part=plogin&username=Znexbf6430&password=',
-	'nonoh':'https://myaccount.nonoh.com/clx/index.php?part=plogin&username=Znexbf6430&password=',	
-	'justvoip':'https://myaccount.justvoip.com/clx/index.php?part=plogin&username=Znexbf6430&password=',
-	'12voip':'https://myaccount.12voip.com/clx/index.php?part=plogin&username=Znexbf6430&password=',	
-	'webcalldirect':'https://myaccount.webcalldirect.com/clx/index.php?part=plogin&username=Znexbf6430&password='
+	'otenet':'http://tools.otenet.gr/tools/index.do'	
 		}	
 acc_opensms = {
 	'otenet':'http://tools.otenet.gr/tools/tiles/web2sms.do?showPage=smsSend&mnu=smenu23',
-	'voipbuster':'https://myaccount.voipbuster.com/clx/index.php?part=menu&justloggedin=true',
-	'voipdiscount':'https://myaccount.voipdiscount.com/clx/index.php?part=menu&justloggedin=true',
-	'lowratevoip':'https://myaccount.lowratevoip.com/clx/index.php?part=menu&justloggedin=true',
-	'voipbusterpro':'https://myaccount.voipbusterpro.com/clx/index.php?part=menu&justloggedin=true',
-	'voipcheap':'https://myaccount.voipcheap.com/clx/index.php?part=menu&justloggedin=true',
-	'nonoh':'https://myaccount.nonoh.com/clx/index.php?part=menu&justloggedin=true',
-	'justvoip':'https://myaccount.justvoip.com/clx/index.php?part=menu&justloggedin=true',
-	'12voip':'https://myaccount.12voip.com/clx/index.php?part=menu&justloggedin=true',
-	'yahoo':'http://everywhere.yahoo.com/sms/sendsms',
-	'webcalldirect':'https://myaccount.webcalldirect.com/clx/index.php?part=menu&justloggedin=true'
+	'voipbuster':'https://myaccount.voipbuster.com/clx/sendsms.php?',
+	'voipdiscount':'https://myaccount.voipdiscount.com/clx/sendsms.php?',
+	'lowratevoip':'https://myaccount.lowratevoip.com/clx/sendsms.php?',
+	'voipbusterpro':'https://myaccount.voipbusterpro.com/clx/sendsms.php?',
+	'freevoip':'https://myaccount.freevoip.com/clx/sendsms.php?',
+	'12voip':'https://myaccount.12voip.com/clx/sendsms.php?',
+	'webcalldirect':'https://myaccount.webcalldirect.com/clx/sendsms.php?',
+	'nonoh':'https://myaccount.nonoh.com/clx/sendsms.php?',
+	'justvoip':'https://myaccount.justvoip.com/clx/sendsms.php?'
 	      }
 
-	
-	
-def usage():                                            
-		print """
-	pysmssend         Send SMS over the Internet
-
-	pysmssend [-h] -a <account> -u <username> -p <password> -n <number> <"text to send">
-	example: pysmssend -a otenet -u foo -p bar -n 123456 "Hello World!"
-	
-  	-h                  print this message
-
-  	-a <account>        Account name: <otenet|voipbuster|voipdiscount|voipbusterpro|lowratevoip|12voip|freevoip|yahoo|webcalldirect>
-  	-u <username>       Username
-  	-p <password>       Password
-  	-n <number>         Telephone Number
-	-v 		    Verbose
-	
-	 Homepage: http://pysmssend.sourceforge.net
-
-		"""
-
-def cmdlogin(account,username,password,verbose):
-	testfoo = Browser()
+def cmdlogin(account,username,password,verbose):#login function for cmd tools
+	#Actually this code applies only to otenet logins.
+	testfoo=Browser()
 	testfoo.set_handle_robots(False)
-	if account!="yahoo":
-		login_page=acc_openlogin.get(account)
-		foobar.addheaders = [("User-agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)")]
-		testfoo.addheaders = [("User-agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)")]
-        	foobar.open(login_page)
-		if account == "otenet":
-			foobar.select_form(name="loginform")
-		if account != "otenet" and account != "yahoo":
-			foobar.select_form(nr=0)
-        	foobar["username"] = username
-               	foobar["password"] = password
+	login_page=acc_openlogin[str(account)]#find url
+	foobar.addheaders = [("User-agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)")]#add eaders
+	testfoo.addheaders = [("User-agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)")]#add headers
+	foobar.open(login_page)#open url
+	foobar.select_form(name="loginform")
+	foobar["username"] = username
+        foobar["password"] = password
+	foobar.submit()
+	if verbose:
+		print "Verifying data.."
+	pass #create a small delay
+	ok=0
+	try:
+		#Try to open sms page (this helps on deciding if login was correct )
+		acc_page="http://tools.otenet.gr/tools/tiles/Intro/generalIntro.do"
 		if verbose:
-	       		print "Verifying Data..."
-		foobar.submit()
-		try:
-			#Try to open sms page (this helps on deciding if login was correct )
-			if account=="otenet":
-				acc_page="http://tools.otenet.gr/tools/tiles/Intro/generalIntro.do"
-			else:
-				acc_page = acc_opensms[str(account)]#find sms page
-			testfoo=foobar
-			foobar.open(acc_page)#open sms page
-			leftcred=cmdcreditsleft(account,testfoo)
-			test=foobar.geturl()
-			if test==acc_page:
-				ok=1
-			else:pass
-		except:
-			sys.exit("Login Failed.")
-		if ok==1:
-			ok=0
+			print "Opening "+acc_page
+		testfoo=foobar
+		foobar.open(acc_page)#open sms page
+		leftcred=creditsleft(account,testfoo)
+		#if all the above worked then the login was ok (i hope so )
+		test=foobar.geturl()
+		if test==acc_page:
 			if verbose:
-	      			print "Login Successfully"
-			leftcred=cmdcreditsleft(account,foobar)
-			if account=="otenet":
-				if verbose: print "Messages left: "+leftcred 
-				if leftcred=="0":
-					sys.exit("Sorry, no sms left to send")
-			if account != "otenet" and account != "yahoo":
-				if verbose: print "Credits :"+leftcred
-				if leftcred<"0.05":
-					sys.exit("Sorry, no credits left on your account")
-	return leftcred			
+				print "Logged in to "+account
+				print "SMS Left: "+leftcred
+		else: 
+			print "Cannot login to "+account
+	except:
+		print "Cannot login to "+account
+	
+					
 
-def sendsmscmd(account,username,password,phone,text,verbose):
-	acc_page = acc_opensms.get(account)
-	if not acc_page:
-		usage()
-		sys.exit("No valid account specified")
-	leftcred=cmdlogin(account,username,password,verbose)
+def sendsmscmd(account,username,password,number,message,verbose):
+	acc_page = acc_opensms[str(account)]#find page
 	foobar.addheaders = [("User-agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)")]
-	foobar.open(acc_page)
-	if account == "otenet":
+	testfoo=foobar
+	if account=="otenet" or account=="yahoo":
+		if verbose:
+			print "Opening "+acc_page
+		foobar.open(acc_page)
+	if account=="otenet":
 		try:
 			foobar.select_form(name="sendform")
-       		except:
-           		sys.exit("Problem filling otenet sms form. Exiting.")
-		foobar["phone"] = phone
-	       	foobar["message"] = text
-	elif account != "otenet"  and account != "yahoo":
-		try:
-		      	foobar.follow_link(text="Send a Text message (SMS)")
-		except:
-			try:
-				foobar.follow_link(text="Send a Text Message (SMS)")
-			except:
-				sys.exit("Problem filling sms form. Exiting.")
-		foobar.select_form(nr=0)  
-		foobar['bnrphonenumber'] = phone
-       		foobar['message'] = text
+			
+     		except:
+        		sys.exit("Error contacting site... Please try again later\n")
+	elif account=="voipbuster" or account=="voipdiscount" or account == "lowratevoip" or account=="voipbusterpro" or account=="freevoip" or account=="12voip" or account=="webcalldirect" or account=="nonoh" or account=="justvoip":
+		#do nothing
+		pass
+		
 	else:#if yahoo
+		try:
+			foobar.select_form(nr=0)  
+		except:
+			sys.exit("Error contacting site... Please try again later\n")
+	if verbose:
+		print "Creating message..."
+	if account=="otenet":
+
+		foobar["phone"] = number
+       		foobar["message"] = message
+	elif account=="yahoo":#this means yahoo
 		foobar["ymsgSmsNumber"] = number
 		foobar["ymsgSmsMessage"] = message
-	if verbose:
-        	print "Sending SMS..."
-       	try:
-        	foobar.submit()
-		#find leftmessages
-		foobar.open(acc_page)
-		cred=cmdcreditsleft(account,foobar)
-		if cred<leftcred:
+	try:
+		if account=="otenet" or account=="yahoo":
 			if verbose:
-           			print "Message Sent."
+				print "Sending..."
+         		foobar.submit()
+			#work around for otenet report
+			#find again the messages left and compare it with the initials
+			# NOT TESTED
+			leftcred2=creditsleft(account,testfoo)
+			if leftcred==leftcred2:#if we have the same messages after the submit
+			#it means that we didnt send the message
+				#set final_report
+				final_report=="failure"
+			#hope it works
 		else:
-			sys.exit("Failure... Couldnt send message")
-       	except:
-        	sys.exit("Failure...Text message too long?")
-	
-		##Credits left
-def cmdcreditsleft(account,foobar):
-	if account != "otenet" and account != "yahoo":
-		gethtml=foobar.response()
-		html=gethtml.read()
-		balance=html.find("balanceid")
-		balanceline=html[balance:]
-		euros=balanceline.split('&nbsp;')
-		creditsleft=euros[1].split('</b>')
-		left=creditsleft[0]
-	elif account=="otenet":
-		foobar.open("http://tools.otenet.gr/tools/tiles/Intro/generalIntro.do")
-		gethtml=foobar.response()
-		html=gethtml.read()
-		balance=html.find("""  <a href="/tools/tiles/web2sms.do?showPage=smsSend&amp;mnu=smenu23"> <span class="txtmov10b_yellow">""")
-		#print balance
-		balanceline=html[balance:]
-		data=balanceline.split()
-		dataline=data[3]
-		temp1=dataline.split("""class="txtmov10b_yellow">""")
-		temp2=temp1[1]
-		left2=temp2.split("</span>")
-		left=left2[0]
-	final=left
-	return final
+			#fixing the url
+			url=acc_opensms[str(account)]
+			if verbose:
+				print "url: "+url
+			#hack 1
+			#adding data
+			values={'username':username,
+				'password':password,
+				'from':username,
+				'to':number,
+				'text':message
+				}
+			if verbose:
+				print "Verifying data"
+			data = urllib.urlencode(values)
+			#adding header
+			user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+			headers = { 'User-Agent' : user_agent }
+			#constructing the url
+			if verbose:
+				print "Sending..."
+			req = urllib2.Request(url, data, headers)
+			#small delay
+			pass
+			pass
+			pass
+			#its not my fault if betamax sends wrong reports
+			response=urllib2.urlopen(req)
+			report=response.read()
+			#lets find out if the message was sent correctly
+			data_response1=report.find("<resultstring>")
+			data_response2=report.find("</resultstring>")
+			final_report=report[data_response1+14:data_response2]
+			#im done with the report.
+	#finish the try except 
+	except:
+		sys.exit("Program terminated with error code")	
+	#finisht the job
+	#if verbose show the report and exit with error code if the report is failure
+	if final_report=="failure":
+		if verbose:
+			print "Report: "+final_report
+		sys.exit("Failed to send message")	
+	else:
+		if verbose:
+			print "Report: "+final_report
+		sys.exit(0)
--- pysmssendmod/mysmssend.py
+++ pysmssendmod/mysmssend.py
@@ -1,87 +1,148 @@
-import sys
 from pysmssendmod.tray import *
 from mechanize import Browser
 from pysmssendmod.creditsleft import *
-
+from pysmssendmod.mywritetofile import *
+import sys
+import urllib2,urllib
+import os,time
+# due to upstream changes , we need to introduce ( again *sigh* ) the old way for sending messages
 acc_opensms = {
 	'otenet':'http://tools.otenet.gr/tools/tiles/web2sms.do?showPage=smsSend&mnu=smenu23',
-	'voipbuster':'https://myaccount.voipbuster.com/clx/index.php?part=menu&justloggedin=true',
-	'voipdiscount':'https://myaccount.voipdiscount.com/clx/index.php?part=menu&justloggedin=true',
-	'lowratevoip':'https://myaccount.lowratevoip.com/clx/index.php?part=menu&justloggedin=true',
-	'voipbusterpro':'https://myaccount.voipbusterpro.com/clx/index.php?part=menu&justloggedin=true',
-	'freevoip':'https://myaccount.freevoip.com/clx/index.php?part=menu&justloggedin=true',
-	'12voip':'https://myaccount.12voip.com/clx/index.php?part=menu&justloggedin=true',
-	'yahoo':'http://everywhere.yahoo.com/sms/sendsms',
-	'webcalldirect':'https://myaccount.webcalldirect.com/clx/index.php?part=menu&justloggedin=true',
-	'voipcheap':'https://myaccount.voipcheap.com/clx/index.php?part=menu&justloggedin=true',
-	'nonoh':'https://myaccount.nonoh.net/clx/index.php?part=menu&justloggedin=true',
-	'justvoip':'https://myaccount.justvoip.com/clx/index.php?part=menu&justloggedin=true'
-	}
-def mysmssend(foobar,f,tray,account,verbose,leftcred):
-	acc_page = acc_opensms[str(account)]#find page
+	'voipbuster':'https://myaccount.voipbuster.com/clx/sendsms.php?username=',
+	'voipdiscount':'https://myaccount.voipdiscount.com/clx/sendsms.php?username=',
+	'lowratevoip':'https://myaccount.lowratevoip.com/clx/sendsms.php?username=',
+	'voipbusterpro':'https://myaccount.voipbusterpro.com/clx/sendsms.php?username=',
+	'freevoip':'https://myaccount.freevoip.com/clx/sendsms.php?username=',
+	'12voip':'https://myaccount.12voip.com/clx/sendsms.php?username=',
+	'webcalldirect':'https://myaccount.webcalldirect.com/clx/sendsms.php?username=',
+	'nonoh':'https://myaccount.nonoh.com/clx/sendsms.php?username=',
+	'justvoip':'https://myaccount.justvoip.com/clx/sendsms.php?username'
+	      }
+def mysmssend(foobar,f,trayic,account):
+	#code is like shit. Sorry about that
+	if account!="NULL":
+		acc_page = acc_opensms[str(account)]#find page
+	else:
+		#find the account by reading the combo again :(
+		account=f.ui.comboBox.currentText()#read account
+		account=str.lower(str(account))
 	foobar.addheaders = [("User-agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)")]
 	testfoo=foobar
-	foobar.open(acc_page)
-	if verbose:
-		print "Im opening -> "+acc_page+" \n"
-	#if we have an otenet account 
-	pass
-	pass
-	if verbose:
-		print "I opened -> "+foobar.geturl()+" \n"
-	if account == "otenet":
-		try:
-			foobar.select_form(name="sendform")
-			
-     		except:
-        		sys.exit("Error contacting site... Please try again later\n")
-	elif account != "otenet" and account != "yahoo":
-		try:
-			foobar.follow_link(text="Send a Text message (SMS)")
-		except:
-			try:
-				foobar.follow_link(text="Send a Text Message (SMS)")
-			except:
-				sys.exit("Cannot open sms page. Please fill a bug ")
-		foobar.select_form(nr=0)  
-	else:#if yahoo
-		try:
-			foobar.select_form(nr=0)  
-		except:
-			sys.exit("Error contacting site... Please try again later\n")
-	#get the number and the message	
+	if account=="otenet" or account=="yahoo":
+		foobar.open(acc_page)
+	#get message and phone and username and password
+	username=f.ui.lineEdit.text()
+        password=f.ui.lineEdit2.text()
 	number=f.ui.lineEdit3.text()
-        message2=f.ui.textEdit.toPlainText()  
+        message2=f.ui.textEdit.toPlainText()
 	#convert Qstring to utf8
 	message=message2.toUtf8()
-	#fill the forms	
+	if account=="otenet":
+		try:
+			foobar.select_form(name="sendform")
+
+     		except:
+        		sys.exit("Error contacting site... Please try again later\n")
+	elif account!="otenet" and account!="yahoo":
+		#do nothing
+		pass
+
+	else:#if yahoo
+		try:
+			foobar.select_form(nr=0)
+		except:
+			sys.exit("Error contacting site... Please try again later\n")
+
+	# passing data
 	if account=="otenet":
 		foobar["phone"] = number
-       		foobar["message"] = message
-	elif account != "otenet" and account != "yahoo":
-		foobar['bnrphonenumber'] =number
-        	foobar['message'] = message
-	elif account == "yahoo" :#this means yahoo
+        foobar["message"] = message
+	elif account=="yahoo":#this means yahoo
 		foobar["ymsgSmsNumber"] = number
 		foobar["ymsgSmsMessage"] = message
-	#we need to convert qstring to plain text
-	message=f.ui.textEdit.toPlainText()  
-	#and then get the size of it
-        size=message.length()
+
+	message=f.ui.textEdit.toPlainText()
+    size=message.length()
 	try:
-		#sumbit the stuff
-         	foobar.submit()
-		if verbose:
-			print "Sending message ... \n"
-		sent=1
+		if account=="otenet" or account=="yahoo":
+         		foobar.submit()
+			sent=1
+		else:
+			#fixing the url
+			url=acc_opensms[str(account)]
+			#hack 1
+			#adding data
+			values={'username':username,
+				'password':password,
+				'from':username,
+				'to':number,
+				'text':message
+				}
+			data = urllib.urlencode(values)
+			#adding header
+			user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+			headers = { 'User-Agent' : user_agent }
+			#constructing the url
+			req = urllib2.Request(url, data, headers)
+			response=urllib2.urlopen(req)
+			#create a 4 seconds delay. If you are using adsl you want have any problems
+			#for those people who use dial up im sorry :P
+			#this delay is to allo urllib2 to give us a proper report of sms
+			time.sleep(4)
+			#im doing this because urllib is kinda stupid and returns false response if i dont add a small delay
+			#duh
+			report=response.read()
+			sent=1
 	except:
-        	f.ui.lineEdit_2.setText("Unknown error occured \n")
+       	f.ui.lineEdit_2.setText("Not sent ...")
 		sent=0
         if sent==1:
-		#re-open the sms page and find credits left
-		foobar.open(acc_page)#re open the sms page
-		cred=creditsleft(f,account,foobar,verbose)
-		if cred<leftcred:
-			tray.showsentreport("Message was sent successfully :) ",1)
-		else: #means something happend
-			tray.showsentreport("Couldnt send message :(",0)
+		if account=="otenet" or account=="yahoo":
+			cred=creditsleft(f,account,testfoo)
+		if account=="otenet":
+			username=f.ui.lineEdit.text()
+			size2=145-len(username)
+			if size<=size2:
+				f.ui.lineEdit_2.setText("Message Sent")
+				showsentreport1(trayic)
+				f.ui.Result1.clear()
+				f.ui.credits.setText("SMS Left : "+str(cred))
+				f.ui.lineEdit_3.clear()
+				f.ui.textEdit.clear()
+			elif size>size2:
+				f.ui.lineEdit_2.setText(" Not sent ...")
+				showsentreport2(trayic)
+		elif account=="voipbuster" or account=="voipdiscount" or account == "lowratevoip" or account=="voipbusterpro" or account=="freevoip" or account=="12voip" or account=="webcalldirect" or account=="justvoip" or account=="nonoh":
+			if size<=160:
+				#now we need to open the response page in order to see if the message was send #succesfully
+				result=report.find("<resultstring>")
+				result2=report.find("</resultstring>")
+				result3=report[result+14:result2]
+				#do the final check
+				#uff
+				print result3
+				if result3=="success":
+					f.ui.lineEdit_2.setText("Message Sent")
+					showsentreport1(trayic)
+					f.ui.credits.setText("Not Available...")
+					f.ui.Result1.clear()
+					f.ui.lineEdit_3.clear()
+					f.ui.textEdit.clear()
+					#save data to file ;)
+					print "Saving account information..."
+					homedir=os.environ["HOME"]
+					if f.ui.rememberMe.checkState()==2:#if Remember Me is ON
+						mywritetofile(account,username,password)#write to file
+				else:
+					f.ui.lineEdit_2.setText(" Not sent ...")
+					showsentreport2(trayic)
+			else:
+				f.ui.lineEdit_2.setText(" Not sent ...")
+				showsentreport2(trayic)
+		else:#this is for yahoo messaging
+			if size<=120:
+				f.ui.lineEdit_2.setText("Message Sent")
+				showsentreport1(trayic)
+				f.ui.Result1.clear()
+				f.ui.lineEdit_3.clear()
+				f.ui.textEdit.clear()
