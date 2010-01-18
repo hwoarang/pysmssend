96ec20fd783ea9aee5253e97a48932981f73a23b
Markos Chandras <hwoarang@gentoo.org>
2010-01-18 16:08:35 +0200

    Replace hardcoded python2.4 versions

--- pysmssendmod/login.py
+++ pysmssendmod/login.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.4
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #***************************************************************************
 #*    Pysmssend ( Source Code ) Login
